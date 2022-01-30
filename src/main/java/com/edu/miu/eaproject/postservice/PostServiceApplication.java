cc1cfcfd70f7f9d8e9be67da255de35c6bf89bde
Demelash Minyahil <dememan@gmail.com>
2022-01-29 23:35:24 -0600

    Eureka client config

--- src/main/java/com/edu/miu/eaproject/postservice/PostServiceApplication.java
+++ src/main/java/com/edu/miu/eaproject/postservice/PostServiceApplication.java
@@ -2,12 +2,14 @@ package com.edu.miu.eaproject.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class PosterviceApplication {
+@EnableEurekaClient
+public class PostServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PosterviceApplication.class, args);
+		SpringApplication.run(PostServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/edu/miu/eaproject/postservice/PostServiceApplicationTests.java
+++ src/test/java/com/edu/miu/eaproject/postservice/PostServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.edu.miu.eaproject.Postervice;
+package com.edu.miu.eaproject.postservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
